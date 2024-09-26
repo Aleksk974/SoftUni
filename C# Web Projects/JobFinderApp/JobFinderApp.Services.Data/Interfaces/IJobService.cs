d7afff11dc354820a729cba7f1fbb3c15b3d695c
Aleksandar Kostadinov <alekskostadinov974@gmail.com>
2024-09-26 07:28:43 +0100

    JobfinderApp upload

---
+++ C# Web Projects/JobFinderApp/JobFinderApp.Services.Data/Interfaces/IJobService.cs
@@ -0,0 +1,21 @@
+﻿using JobFinderApp.Web.ViewModels.Job;
+
+namespace JobFinderApp.Services.Data.Interfaces
+{
+    public interface IJobService
+    {
+        Task<IEnumerable<AllJobsViewModel>> GetAllJobsAsync();
+
+        Task<JobViewModel> GetNewAddJobAsync();
+
+        Task AddJobAsync(JobViewModel model, string userId);
+
+        Task<JobViewModel?> GetJobByIdForEdit(int id);
+
+        Task EditJobAsync(JobViewModel model, string userId);
+
+        Task DeleteJobByIdAsync(int id);
+
+        Task<JobDescriptionViewModel> JobDetailsAsync(int id);
+    }
+}
